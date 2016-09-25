87b10e70f543986cb0f4dd2afb3a06738104df83
Steven <stevenpantaleo@hotmail.com>
2016-09-24 21:50:25 -0400

    Moved db conn info out

--- index.php
+++ index.php
@@ -7,21 +7,9 @@
 
 <body>
   <?php
-
+    require "dbconn.php";
   //PDO
-  $host = 'x';
-  $db   = 'x';
-  $user = 'x';
-  $pass = 'x';
-  $charset = 'x';
 
-  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-  //$opt = [
-  //    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-  //    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-  //    PDO::ATTR_EMULATE_PREPARES   => false,
-  //];
-  $pdo = new PDO($dsn, $user, $pass);
   $stmt = $pdo->query('SELECT username, password FROM users');
   while ($row = $stmt->fetch()){
           echo $row['username']. " | ". $row['password']."<br/>";
